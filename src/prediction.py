bba4c8e6376c8ee44a7a7ce5e67e035918e37c36
prngx <sochorm@outlook.com>
2023-07-27 09:56:00 +0200

    updated folder structure, added new files

---
+++ src/prediction.py
@@ -0,0 +1,4 @@
+# contains all the code used to predict a new house's price.
+
+def predict_new_data():
+    return price
---
+++ src/preprocessing.py
@@ -0,0 +1,6 @@
+#preprocessing the user input data before using them in the trained model
+
+def preprocess_new_data(datainput):
+
+    
+    return dataoutput
